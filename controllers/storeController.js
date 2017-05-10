b6d5a3977236134002250be0368b2aaa62a9592f
Jacob Price <Jacob@Price.local>
2017-05-10 07:05:14 -0400

    Adding flash messaging, edit store functioning

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,8 +10,27 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body)
-    await store.save()
-    res.redirect('/')
-    console.log('It worked!')
+    const store = await (new Store(req.body)).save()
+    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
+    res.redirect('/store/${store.slug}')
+}
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find()
+    res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async(req, res) => {
+    const store = await Store.findOne({ _id: req.params.id })
+    res.render('editStore', { title: `Edit ${store.name}`, store})
+}
+
+exports.updateStore = async(req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, //return the new store instead of the old One
+        runValidators: true
+    }).exec()
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`)
+    res.redirect(`/stores/${store._id}/edit`)
+    //Redirecting to the store and telling them it worked 
 }
